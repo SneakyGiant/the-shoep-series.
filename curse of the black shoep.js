53021aa6cef56e5c95960935dacad7b256e7ce79
PandaStew <51493545+PandaStew@users.noreply.github.com>
2022-05-10 13:15:50 +0200

    1

--- curse of the black shoep.js
+++ curse of the black shoep.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
-  
+  $(".shoep").hide();
+
+  $("#img").mouseenter(function(){
+    $(".shoep").show(1000);
+    alert("you found shoep");
+    console.log("crap");
+  });
+
+  $("#img").mouseleave(function(){
+    $(".shoep").hide(1000);
+    console.log("fuck");
+  });
+
+alert("shoep is hidden");
+
+
 });
