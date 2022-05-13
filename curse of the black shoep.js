fe9c5c8ec30826a65b76ed761a00d545d10b05c4
PandaStew <51493545+PandaStew@users.noreply.github.com>
2022-05-13 12:42:43 +0200

    1

--- curse of the black shoep.js
+++ curse of the black shoep.js
@@ -6,5 +6,11 @@ $(document).ready(function(){
   $("#add").click(function(){
     var Toevoegen = $("#txt").val();
     $(".links").text(Toevoegen);
+    Links[Links.length] = Toevoegen;
+
+    $(".links").empty();
+    for(i = 0; i < Links.length; i++){
+      
+    }
   });
 });
