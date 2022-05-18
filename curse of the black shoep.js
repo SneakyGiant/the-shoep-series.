431dcacd8173e98b875879905e843ee6c89d464d
PandaStew <51493545+PandaStew@users.noreply.github.com>
2022-05-18 13:17:27 +0200

    final
    final

--- curse of the black shoep.js
+++ curse of the black shoep.js
@@ -2,6 +2,9 @@ $(document).ready(function(){
 
   var Links = [];
   var Rechts = [];
+  var selectedNumber;
+  var selectedList;
+  var WordId;
 
   $("#add").click(function(){
     var Toevoegen = $("#txt").val();
@@ -9,9 +12,76 @@ $(document).ready(function(){
     Links[Links.length] = Toevoegen;
 
     $(".links").empty();
-    for(i = 0; i < Links.length; i++){
-      $(".links").append('<div class="word" data-list="left" data-number="'+i+'">'+leftList[i]+'</div>');
+    for(var i = 0; i < Links.length; i++){
+      $(".links").append('<div class="word" data-list="left" data-number="'+i+'">'+Links[i]+'</div>');
     };
-    
+
+    $(".rechts").empty();
+    for(var i = 0; i < Rechts.length; i++){
+      $(".rechts").append('<div class="word" data-list="right" data-number="'+i+'">'+Rechts[i]+'</div>');
+    };
+
+    $('.word').click(function() {
+        $('.selectedWord').removeClass('selectedWord');
+        $(this).toggleClass('selectedWord');
+        WordId = $(this).attr('id');
+        selectedList = $(this).attr('data-list');
+        selectedNumber = $(this).attr('data-number');
+    });
   });
+  
+  $('#toRight').click(function() {
+
+      if($('.word').hasClass('selectedWord') && selectedList == "left") {
+
+          Rechts.push(Links[selectedNumber]);
+          Links.splice(selectedNumber, 1);
+
+          $('.links').empty();
+          for(var i = 0; i < Links.length; i++) {
+              $('.rechts').append('<div class="word" data-list="left" data-number="'+i+'">'+Links[i]+'</div>');
+          };
+
+          $('.rechts').empty();
+          for(var i = 0; i < Rechts.length; i++) {
+              $('.rechts').append('<div class="word" data-list="right" data-number="'+i+'">'+Rechts[i]+'</div>');
+          };
+
+          $('.word').click(function() {
+              $('.selectedWord').removeClass('selectedWord');
+              $(this).toggleClass('selectedWord');
+              WordId = $(this).attr('id');
+              selectedList = $(this).attr('data-list');
+              selectedNumber = $(this).attr('data-number');
+          });
+      };
+  });
+  $('#toLeft').click(function() {
+
+      if($('.word').hasClass('selectedWord') && selectedList == "right") {
+
+          Links.push(Rechts[selectedNumber]);
+          Rechts.splice(selectedNumber, 1);
+
+          $('.links').empty();
+          for(var i = 0; i < Links.length; i++) {
+              $('.links').append('<div class="word" data-list="left" data-number="'+i+'">'+Links[i]+'</div>');
+          };
+
+
+          $('.rechts').empty();
+          for(var i = 0; i < Rechts.length; i++) {
+              $('.rechts').append('<div class="word" data-list="right" data-number="'+i+'">'+Rechts[i]+'</div>');
+          };
+
+          $('.word').click(function() {
+              $('.selectedWord').removeClass('selectedWord');
+              $(this).toggleClass('selectedWord');
+              WordId = $(this).attr('id');
+              selectedList = $(this).attr('data-list');
+              selectedNumber = $(this).attr('data-number');
+          });
+      };
+  });
+
 });
