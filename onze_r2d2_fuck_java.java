cb0e1b9bf9eb680a2a825c85735d57266a690d00
tarohoek <91067554+tarohoek@users.noreply.github.com>
2022-06-01 14:56:27 +0200

    Update onze_r2d2_fuck_java.java

--- onze_r2d2_fuck_java.java
+++ onze_r2d2_fuck_java.java
@@ -1,17 +1,27 @@
 public class MyClass {
-    public static void main(String args[]) {
-        int n = (int)(Math.random() * 10);
-        String[] words = {"nanny", "headphones"}; //hier moeten stings komen
-        String chosenword = words[n];
-        String correctletters = "";
-        for(int x = 0; x<chosenword.length(); x++){
-            correctletters = correctletters + "_ ";
-        }
-        for(int y = 0; y<args[0].length(); y++){
-        	if (args[0].charAt(y) = chosenword.charAt(y)){
-                correctletters.replace(correctletters.charAt(y), args[0].charAt(y));
-            }
-        }
+	public static void main(String args[]) {
+		String correctletters = "";
+		String chosenword = "";
+		String[] words = {"nanny", "headphones"}; //hier moeten stings komen
+		int anders_werkt_deze_bullshit_niet = 0;
+		
+		if (anders_werkt_deze_bullshit_niet == 0) {
+	        int n = (int)(Math.random() * 2);        
+	        chosenword = words[n];
+	        for(int x = 0; x<chosenword.length(); x++){
+	            correctletters = correctletters + "_ ";
+	        }
+		}
+		
+		if (chosenword.contains(args[0])) {
+			 for(int y = 0; y<args[0].length(); y++){
+		        	if (args[0].charAt(y) == chosenword.charAt(y)){
+		                correctletters.replace(correctletters.charAt(y), args[0].charAt(y));
+		            }
+		        }
+		}
+		
+       
         if (args[0] == chosenword){
             System.out.println("Correct guess");
         }
