9c58ef9524226779bbe7b8c9b4d357c743dc834b
tarohoek <91067554+tarohoek@users.noreply.github.com>
2022-06-01 12:50:44 +0200

    Update D2.java

--- D2.java
+++ D2.java
@@ -1,20 +1,23 @@
 public class D2 {
     public static void main(String[] args) {
-        
-    int n = (int)(Math.random() * 10);
-    String[] words = {nanny}; //hier moeten stings komen
-        String chosenword = words[1];
+
+        int n = (int)(Math.random() * 10);
+        String[] words = {"nanny"}; //hier moeten stings komen
+        String chosenword = words[0];
         String correctletters = "";
-            for(int x = 0; x<chosenword.length; x++){
-                correctletters = corectletters + "_"
-                    if (args[0].charAt(y) == chosenword.charAt(y){
-                        correctletters.replace(correctletters.charAt(y), args[0].charAt(y));
-                  }
+        for(int x = 0; x<chosenword.length(); x++){
+            correctletters = correctletters + "_";
+        }
+        for(int y = 0; y<args[0].length(); y++){
+        	if (args[0].charAt(y) == chosenword.charAt(y)){
+                correctletters.replace(correctletters.charAt(y), args[0].charAt(y));
             }
+        }
         if (args[0] == chosenword){
             System.out.println("Correct guess");
         }
         else{
             System.out.println(correctletters);
         }
+    }
 }
