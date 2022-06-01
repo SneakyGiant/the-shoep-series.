f535a9d17e755dfefb5bd338c863704cfed82261
tarohoek <91067554+tarohoek@users.noreply.github.com>
2022-06-01 14:24:07 +0200

    Update onze_r2d2_fuck_java.java

--- onze_r2d2_fuck_java.java
+++ onze_r2d2_fuck_java.java
@@ -1,14 +1,14 @@
 public class MyClass {
     public static void main(String args[]) {
         int n = (int)(Math.random() * 10);
-        String[] words = {"nanny"}; //hier moeten stings komen
-        String chosenword = words[0];
+        String[] words = {"nanny", "headphones"}; //hier moeten stings komen
+        String chosenword = words[n];
         String correctletters = "";
         for(int x = 0; x<chosenword.length(); x++){
             correctletters = correctletters + "_ ";
         }
         for(int y = 0; y<args[0].length(); y++){
-        	if (args[0].charAt(y) == chosenword.charAt(y)){
+        	if (args[0].charAt(y) = chosenword.charAt(y)){
                 correctletters.replace(correctletters.charAt(y), args[0].charAt(y));
             }
         }
