e459be8d2b3495a43d5bc2a2c75ec14df7a575ce
PandaStew <51493545+PandaStew@users.noreply.github.com>
2022-06-07 09:31:55 +0200

    nightmare
    nightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmarenightmare

---
+++ D2javamaardanincppwantjavaisoutdated.cpp
@@ -0,0 +1,18 @@
+/*
+ * D2javamaardanincppwantjavaisoutdated.cpp
+ *
+ *  Created on: Jun 7, 2022
+ *      Author: thier
+ */
+
+#include "D2javamaardanincppwantjavaisoutdated.h"
+
+D2_java_maar_dan_in_cpp_want_java_is_outdated::D2_java_maar_dan_in_cpp_want_java_is_outdated() {
+	// TODO Auto-generated constructor stub
+
+}
+
+D2_java_maar_dan_in_cpp_want_java_is_outdated::~D2_java_maar_dan_in_cpp_want_java_is_outdated() {
+	// TODO Auto-generated destructor stub
+}
+
