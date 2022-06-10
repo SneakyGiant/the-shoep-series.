603ac3e5804c13b0653c5b458a1231c7b0cb14cc
tarohoek <91067554+tarohoek@users.noreply.github.com>
2022-06-10 09:54:02 +0200

    Update D2.java

--- D2.java
+++ D2.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 public class D2 {
 	public static void main(String[] args) {
 		String chosenword = "";
-		String[] words = {"nanny", "headphones"};
+		String[] words = {"nanny", "headphones", "background", "computer", "submarine"};
 		String correctletters = "";
-		int n = (int) (Math.random() * 2);
+		int n = (int) (Math.random() * 5);
 		chosenword = words[n];
 		for (int y=0; y<chosenword.length(); y++) {
 			correctletters = correctletters + "_ ";
@@ -18,7 +18,7 @@ static void function(String chosenword, String correctletters) {
 	String input = inputread.nextLine();
 	
 	
-	if (chosenword == input) {
+	if (input.equals(chosenword)) {
 		System.out.println("correct guess");
 		inputread.close();
 	}
@@ -27,12 +27,12 @@ static void function(String chosenword, String correctletters) {
 			
 			
 			int i = chosenword.indexOf(input);
-			while(i <= chosenword.length()) {
+			while(i <= chosenword.length() && i != -1) {
 				String a = correctletters.substring(0, 2*i);
 				char b = input.charAt(0);
 				String c = correctletters.substring(2*i+1);
 				correctletters = a + b + c;
-			     i = chosenword.indexOf(input, i);
+			     i = chosenword.indexOf(input, i+1);
 			}
 			System.out.println(correctletters);
 			function(chosenword, correctletters);
@@ -41,7 +41,6 @@ static void function(String chosenword, String correctletters) {
 			System.out.println(correctletters);
 			function(chosenword, correctletters);
 		}
-	}
-	
-	}
+	}	
+}
 }
