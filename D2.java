9f8453a1eba48d427141772fdf818bb59837b59c
tarohoek <91067554+tarohoek@users.noreply.github.com>
2022-06-10 10:46:41 +0200

    Update D2.java

--- D2.java
+++ D2.java
@@ -1,19 +1,21 @@
 import java.util.Scanner;
 public class D2 {
+
 	public static void main(String[] args) {
 		String chosenword = "";
 		String[] words = {"nanny", "headphones", "background", "computer", "submarine"};
 		String correctletters = "";
+		int wrong = 0;
 		int n = (int) (Math.random() * 5);
 		chosenword = words[n];
 		for (int y=0; y<chosenword.length(); y++) {
 			correctletters = correctletters + "_ ";
 		}
-		function(chosenword, correctletters);		
+		function(chosenword, correctletters, wrong);		
 	}
 
 
-static void function(String chosenword, String correctletters) {
+static void function(String chosenword, String correctletters, int wrong) {
 	final Scanner inputread = new Scanner(System.in);
 	String input = inputread.nextLine();
 	
@@ -22,10 +24,11 @@ static void function(String chosenword, String correctletters) {
 		System.out.println("correct guess");
 		inputread.close();
 	}
+	else if(wrong == 9){
+		System.out.println("you've lost");
+	}
 	else {
 		if(chosenword.contains(input)) {
-			
-			
 			int i = chosenword.indexOf(input);
 			while(i <= chosenword.length() && i != -1) {
 				String a = correctletters.substring(0, 2*i);
@@ -35,11 +38,14 @@ static void function(String chosenword, String correctletters) {
 			     i = chosenword.indexOf(input, i+1);
 			}
 			System.out.println(correctletters);
-			function(chosenword, correctletters);
+			System.out.println(9-wrong + " wrong guesses left");
+			function(chosenword, correctletters, wrong);
 		}
 		else {
+			wrong++;
 			System.out.println(correctletters);
-			function(chosenword, correctletters);
+			System.out.println(9-wrong + " wrong guesses left");
+			function(chosenword, correctletters, wrong);
 		}
 	}	
 }
