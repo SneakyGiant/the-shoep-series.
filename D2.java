b3af9b131603ccbb7169cdcb19094f66942bd9ca
tarohoek <91067554+tarohoek@users.noreply.github.com>
2022-06-24 12:42:17 +0200

    Update D2.java

--- D2.java
+++ D2.java
@@ -1,7 +1,7 @@
-import javax.swing.*;
+import java.awt.*;
 import java.awt.event.*;
 import java.util.ArrayList;
-import java.awt.FlowLayout;
+import javax.swing.*;
     public class D2 {  
     	public static String correctletters = "";
     	public static String chosenword = "";
@@ -19,27 +19,63 @@ import java.awt.FlowLayout;
 		for (int y=0; y<chosenword.length(); y++) {
 			correctletters = correctletters + "_ ";
 		}
+	GridBagLayout layout = new GridBagLayout();
 	JFrame.setDefaultLookAndFeelDecorated(true);
     JFrame frame=new JFrame();
 
     JPanel panel = new JPanel();
-    panel.setLayout(new FlowLayout());
-    JTextField top_text = new JTextField("a", 20);         
-    JButton buton=new JButton("click");
-    buton.setBounds(130,100,100, 40);
+    panel.setLayout(new GridBagLayout());
+    GridBagConstraints gbc = new GridBagConstraints();
+    
+    JTextField top_text = new JTextField(20);
+    gbc.gridx = 0;
+    gbc.gridy = 0;
+    gbc.fill = GridBagConstraints.VERTICAL;
+    layout.setConstraints(top_text, gbc);
+    
+    JButton buton=new JButton("check");
+    buton.setSize(250, 20);
+    gbc.gridx = 0;
+    gbc.gridy = 1;
+    layout.setConstraints(buton, gbc);
+    
+    JButton nieuw = new JButton("new game");
+    nieuw.setSize(250, 20);
+    gbc.gridx = 1;
+    gbc.gridy = 1;  
+    layout.setConstraints(nieuw, gbc);
     
     JTextField wg = new JTextField(String.valueOf(wrong),20);
-    JTextField wl = new JTextField(list, 20);
-    JTextField output = new JTextField(correctletters,20);
+    gbc.gridx = 0;
+    gbc.gridy = 2;
+    layout.setConstraints(wg, gbc);
     
+    JTextField wl = new JTextField(list, 20);
+    gbc.gridx = 0;
+    gbc.gridy = 3;
+    layout.setConstraints(wl, gbc);
+    
+    JTextField output = new JTextField(correctletters);
+    gbc.gridx = 0;
+    gbc.gridy = 4; 
+    layout.setConstraints(output, gbc);
+    
+    nieuw.addActionListener(new ActionListener() {
+    	public void actionPerformed(ActionEvent e) {
+    		D2.main(null);
+    	}
+    });
     buton.addActionListener(new ActionListener(){
     	public void actionPerformed(ActionEvent e) {
     		list = String.join(", ", wrongletters);
-    		String input = top_text.getText();   		
+    		String input = top_text.getText(); 
+    		
     		function(output, wl, wg ,input, chosenword, correctletters, wrong, wrongletters);
+    		
     	}
     });
     panel.add(top_text);
+    panel.add(nieuw);
     panel.add(buton);
     panel.add(wg);
     panel.add(wl);
@@ -48,14 +84,13 @@ import java.awt.FlowLayout;
     frame.pack();
     frame.setTitle("Shoup 2 electric boogaloo");          
  	frame.setSize(420,420); 
- 	frame.setResizable(false);
     frame.setLayout(null);
     frame.setVisible(true);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     
     }  
     static void function(JTextField output, JTextField wl, JTextField wg, String input, String chosenword, String correctletters, int wrong, ArrayList<String> wrongletters) {
-    	
+    	wrong = wrongletters.size();
     	if (input.equals(chosenword) || !correctletters.contains("_")) {
     		wg.setText("correct guess");
 			wl.setText("");
@@ -77,32 +112,34 @@ import java.awt.FlowLayout;
     				}
     				output.setText(correctletters);
     				if (correctletters.contains("_")) {
+    					
     					wg.setText(10-wrong + " wrong guesses left");
     					wl.setText("wrong guesses "+wrongletters);
+    					output.setText(correctletters);
 
     				}
     				else {
     					wg.setText("correct guess");
     					wl.setText("");
+    					output.setText(correctletters);
     				}
     			}
     			else {
-    				wrong++;
     				wrongletters.add(input);
-    				output.setText(correctletters);
+    							
     				wg.setText(10-wrong + " wrong guesses left");
     				wl.setText("wrong guesses "+wrongletters);
-
+    				output.setText(correctletters);
     			}
     		}
     		else {
-    			wrong++;
     			wrongletters.add(input);
-    			output.setText(correctletters);		
+    					
     			wg.setText(10-wrong + " wrong guesses left");
     			wl.setText("wrong guesses "+wrongletters);
+    			output.setText(correctletters);
     		}
-    		System.out.println(wrong);
     	}
+    	D2.correctletters = correctletters;
     }
-   
+    }
